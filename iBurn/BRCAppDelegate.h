19d43bfac96f120f0c62ab29d2e1a54f052e7afb
David Chiles <dwalterc@gmail.com>
2014-07-28 16:30:17 -0700

    Created new xcodeproj

---
+++ iBurn/BRCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BRCAppDelegate.h
+//  iBurn
+//
+//  Created by David Chiles on 7/28/14.
+//  Copyright (c) 2014 Burning Man Earth. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BRCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
