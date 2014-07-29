c8016e855148804d7adfba216e46307dcc2a6b6c
Chris Ballinger <chrisballinger@gmail.com>
2014-07-29 10:06:24 -0700

    Initial view mappings

--- iBurn/BRCArtTableViewController.m
+++ iBurn/BRCArtTableViewController.m
@@ -9,13 +9,16 @@
 #import "BRCArtTableViewController.h"
 #import "BRCDatabaseManager.h"
 #import "YapDatabaseViewTransaction.h"
+#import "YapDatabaseViewMappings.h"
+#import "YapDatabaseViewConnection.h"
 #import "BRCArtObject.h"
 
 static NSString *const BRCArtTableViewCellIdentifier = @"BRCArtTableViewCellIdentifier";
 
 
 @interface BRCArtTableViewController ()
-
+@property (nonatomic, strong) YapDatabaseViewMappings *mappings;
+@property (nonatomic, strong) YapDatabaseConnection *databaseConnection;
 @end
 
 @implementation BRCArtTableViewController
@@ -35,33 +38,134 @@ static NSString *const BRCArtTableViewCellIdentifier = @"BRCArtTableViewCellIden
     // Do any additional setup after loading the view.
     
     [self.tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:BRCArtTableViewCellIdentifier];
-}
+    
+    self.databaseConnection = [[BRCDatabaseManager sharedInstance].database newConnection];
+    [self.databaseConnection beginLongLivedReadTransaction];
+    
+    self.mappings = [[YapDatabaseViewMappings alloc] initWithGroups:@[[BRCArtObject collection]] view:BRCArtDatabaseViewExtensionName];
 
-- (void) viewWillAppear:(BOOL)animated {
-    [super viewWillAppear:animated];
-    [self.tableView reloadData];
-}
-
-////// Required //////
-- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
-{
-    __block NSInteger numberOfRows = 0;
-    [[BRCDatabaseManager sharedInstance].mainThreadReadOnlyDatabaseConnection readWithBlock:^(YapDatabaseReadTransaction *transaction) {
-        numberOfRows = [[transaction ext:BRCArtDatabaseViewExtensionName] numberOfKeysInGroup:[BRCArtObject collection]];
+    [self.databaseConnection readWithBlock:^(YapDatabaseReadTransaction *transaction){
+        // One-time initialization
+        [self.mappings updateWithTransaction:transaction];
     }];
-    return numberOfRows;
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(yapDatabaseModified:)
+                                                 name:YapDatabaseModifiedNotification
+                                               object:self.databaseConnection.database];
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:BRCArtTableViewCellIdentifier forIndexPath:indexPath];
     __block BRCArtObject *artObject = nil;
-    [[BRCDatabaseManager sharedInstance].mainThreadReadOnlyDatabaseConnection readWithBlock:^(YapDatabaseReadTransaction *transaction) {
-        artObject = [[transaction ext:BRCArtDatabaseViewExtensionName] objectAtIndex:indexPath.row inGroup:[BRCArtObject collection]];
+    [self.databaseConnection readWithBlock:^(YapDatabaseReadTransaction *transaction) {
+        artObject = [[transaction extension:BRCArtDatabaseViewExtensionName] objectAtIndexPath:indexPath withMappings:self.mappings];
     }];
     cell.textLabel.text = artObject.title;
     cell.detailTextLabel.text = artObject.detailDescription;
     return cell;
+}
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)sender
+{
+    return [self.mappings numberOfSections];
+}
+
+- (NSInteger)tableView:(UITableView *)sender numberOfRowsInSection:(NSInteger)section
+{
+    return [self.mappings numberOfItemsInSection:section];
+}
+
+- (void)yapDatabaseModified:(NSNotification *)notification
+{
+    // Jump to the most recent commit.
+    // End & Re-Begin the long-lived transaction atomically.
+    // Also grab all the notifications for all the commits that I jump.
+    // If the UI is a bit backed up, I may jump multiple commits.
+    
+    NSArray *notifications = [self.databaseConnection beginLongLivedReadTransaction];
+    
+    // Process the notification(s),
+    // and get the change-set(s) as applies to my view and mappings configuration.
+    
+    NSArray *sectionChanges = nil;
+    NSArray *rowChanges = nil;
+    
+    [[self.databaseConnection ext:BRCArtDatabaseViewExtensionName] getSectionChanges:&sectionChanges
+                                                  rowChanges:&rowChanges
+                                            forNotifications:notifications
+                                                withMappings:self.mappings];
+    
+    // No need to update mappings.
+    // The above method did it automatically.
+    
+    if ([sectionChanges count] == 0 & [rowChanges count] == 0)
+    {
+        // Nothing has changed that affects our tableView
+        return;
+    }
+    
+    // Familiar with NSFetchedResultsController?
+    // Then this should look pretty familiar
+    
+    [self.tableView beginUpdates];
+    
+    for (YapDatabaseViewSectionChange *sectionChange in sectionChanges)
+    {
+        switch (sectionChange.type)
+        {
+            case YapDatabaseViewChangeDelete :
+            {
+                [self.tableView deleteSections:[NSIndexSet indexSetWithIndex:sectionChange.index]
+                              withRowAnimation:UITableViewRowAnimationAutomatic];
+                break;
+            }
+            case YapDatabaseViewChangeInsert :
+            {
+                [self.tableView insertSections:[NSIndexSet indexSetWithIndex:sectionChange.index]
+                              withRowAnimation:UITableViewRowAnimationAutomatic];
+                break;
+            }
+            default:
+                break;
+        }
+    }
+    
+    for (YapDatabaseViewRowChange *rowChange in rowChanges)
+    {
+        switch (rowChange.type)
+        {
+            case YapDatabaseViewChangeDelete :
+            {
+                [self.tableView deleteRowsAtIndexPaths:@[ rowChange.indexPath ]
+                                      withRowAnimation:UITableViewRowAnimationAutomatic];
+                break;
+            }
+            case YapDatabaseViewChangeInsert :
+            {
+                [self.tableView insertRowsAtIndexPaths:@[ rowChange.newIndexPath ]
+                                      withRowAnimation:UITableViewRowAnimationAutomatic];
+                break;
+            }
+            case YapDatabaseViewChangeMove :
+            {
+                [self.tableView deleteRowsAtIndexPaths:@[ rowChange.indexPath ]
+                                      withRowAnimation:UITableViewRowAnimationAutomatic];
+                [self.tableView insertRowsAtIndexPaths:@[ rowChange.newIndexPath ]
+                                      withRowAnimation:UITableViewRowAnimationAutomatic];
+                break;
+            }
+            case YapDatabaseViewChangeUpdate :
+            {
+                [self.tableView reloadRowsAtIndexPaths:@[ rowChange.indexPath ]
+                                      withRowAnimation:UITableViewRowAnimationNone];
+                break;
+            }
+        }
+    }
+    
+    [self.tableView endUpdates];
 }
 
 @end
