679324c38da34e6139688a340dca22942f9cf85b
Chris Ballinger <chrisballinger@gmail.com>
2019-08-20 21:31:09 -0700

    Update audio tour URL

--- iBurn/AudioTourViewController.swift
+++ iBurn/AudioTourViewController.swift
@@ -98,7 +98,7 @@ class AudioTourViewController: SortedViewController {
 
 private extension AudioTourViewController {
     @objc func soundcloudButtonPressed(_ sender: Any) {
-        let url = URL(string: "https://soundcloud.com/burningman/sets/2018-burning-man-art-audio-guide-i-robot")!
+        let url = URL(string: "https://soundcloud.com/burningman/sets/audio-art-tour-2019")!
         WebViewHelper.presentWebView(url: url, from: self)
     }
 }
