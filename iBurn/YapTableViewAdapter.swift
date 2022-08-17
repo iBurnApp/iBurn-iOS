3797ed4a7aeea42ee54d1a96c6ce6ed1fafa7564
Chris Ballinger <chrisballinger@gmail.com>
2022-08-16 20:28:56 -0700

    Attempt to use DiffableDataSource

--- iBurn/YapTableViewAdapter.swift
+++ iBurn/YapTableViewAdapter.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import UIKit
 
 @objc public protocol YapTableViewAdapterDelegate: NSObjectProtocol {
     func didSelectObject(_ adapter: YapTableViewAdapter,
@@ -19,19 +20,40 @@ import Foundation
     var tableView: UITableView {
         didSet {
             tableView.delegate = self
-            tableView.dataSource = self
+            tableView.dataSource = dataSource
         }
     }
     let viewHandler: YapViewHandler
     let writeConnection: YapDatabaseConnection
     /// For converting sectionIndexTitles
-    public var groupTransformer: (String) -> String = { $0 }
+    public var groupTransformer: (String) -> String = { $0 } {
+        didSet {
+            dataSource.groupTransformer = groupTransformer
+        }
+    }
     public weak var delegate: YapTableViewAdapterDelegate?
     var audioObserver: NSObjectProtocol?
     /// on the right side quick scroll bar
-    var showSectionIndexTitles = true
+    var showSectionIndexTitles = true {
+        didSet {
+            dataSource.showSectionIndexTitles = showSectionIndexTitles
+        }
+    }
     /// header sections
-    var showSectionHeaderTitles = false
+    var showSectionHeaderTitles = false {
+        didSet {
+            dataSource.showSectionHeaderTitles = showSectionHeaderTitles
+        }
+    }
+    private lazy var dataSource = DiffableDataSource(viewHandler: viewHandler, tableView: tableView) {
+        let tableView = $0
+        let indexPath = $1
+        let _ = $2
+        guard let object = self.viewHandler.dataObjectAtIndexPath(indexPath) else {
+            return nil
+        }
+       return BRCDataObjectTableViewCell.cell(at: indexPath, tableView: tableView, dataObject: object, writeConnection: self.writeConnection)
+    }
 
     /// This will take control of the UITableViewDataSource
     /// and YapViewHandlerDelegate
@@ -42,8 +64,8 @@ import Foundation
         self.viewHandler = viewHandler
         self.writeConnection = writeConnection
         super.init()
-        tableView.dataSource = self
         tableView.delegate = self
+        tableView.dataSource = dataSource
         viewHandler.delegate = self
         setupNotifications()
     }
@@ -68,15 +90,151 @@ extension YapTableViewAdapter: UITableViewDelegate {
         tableView.deselectRow(at: indexPath, animated: true)
     }
 }
+//
+//extension YapTableViewAdapter: UITableViewDataSource {
+//
+//    public func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+//        guard showSectionHeaderTitles else { return nil }
+//        return groupTransformer(viewHandler.allGroups[section])
+//    }
+//
+//    public func sectionIndexTitles(for tableView: UITableView) -> [String]? {
+//        guard showSectionIndexTitles else {
+//            return nil
+//        }
+//        let groups = viewHandler.allGroups.map {
+//            groupTransformer($0)
+//        }
+////        groups.insert(UITableViewIndexSearch, at: 0)
+//        return groups
+//    }
+//
+////    public func tableView(_ tableView: UITableView, sectionForSectionIndexTitle title: String, at index: Int) -> Int {
+////        if index > 0 {
+////            return viewHandler.sectionForGroup(title)
+////        } else {
+////            tableView.scrollRectToVisible(CGRect(x: 0, y: 0, width: 1, height: 1), animated: false)
+////            return NSNotFound
+////        }
+////    }
+//
+//    public func numberOfSections(in tableView: UITableView) -> Int {
+//        return viewHandler.numberOfSections
+//    }
+//
+//    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return viewHandler.numberOfItemsInSection(section)
+//    }
+//
+//    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        guard let object = viewHandler.dataObjectAtIndexPath(indexPath) else {
+//            return UITableViewCell()
+//        }
+//        let cell = BRCDataObjectTableViewCell.cell(at: indexPath, tableView: tableView, dataObject: object, writeConnection: writeConnection)
+//        return cell
+//    }
+//}
 
-extension YapTableViewAdapter: UITableViewDataSource {
+extension YapTableViewAdapter: YapViewHandlerDelegate {
+    public func didSetupMappings(_ handler: YapViewHandler) {
+        var snapshot = NSDiffableDataSourceSnapshot<Int, IndexPath>()
+
+        let sections = Array(0..<handler.numberOfSections)
+        snapshot.appendSections(sections)
+        
+        for section in sections {
+            let numberOfItems = handler.numberOfItemsInSection(section)
+            let rows = (0..<numberOfItems).map {
+                IndexPath(row: $0, section: section)
+            }
+            snapshot.appendItems(rows, toSection: section)
+        }
+        
+        dataSource.apply(snapshot, animatingDifferences: false)
+    }
     
-    public func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+    public func didReceiveChanges(_ handler: YapViewHandler, sectionChanges: [YapDatabaseViewSectionChange], rowChanges: [YapDatabaseViewRowChange]) {
+        var snapshot = self.dataSource.snapshot()
+        
+        let updates = {
+            sectionChanges.forEach {
+                switch $0.type {
+                case .insert:
+                    snapshot.appendSections([Int($0.index)])
+                case .delete:
+                    snapshot.deleteSections([Int($0.index)])
+                case .move, .update:
+                    // moves and updates are not supported
+                    break
+                @unknown default:
+                    break
+                }
+            }
+            rowChanges.forEach {
+                switch $0.type {
+                case .insert:
+                    if let newIndexPath = $0.newIndexPath {
+                        let identifiers = snapshot.itemIdentifiers(inSection: newIndexPath.section)
+                        if let lastItem = identifiers.last {
+                            snapshot.insertItems([newIndexPath], afterItem: lastItem)
+                        }
+                    }
+                case .delete:
+                    if let indexPath = $0.indexPath {
+                        snapshot.deleteItems([indexPath])
+                    }
+                case .move:
+                    if let _ = $0.indexPath,
+                        let newIndexPath = $0.newIndexPath {
+                        let identifiers = snapshot.itemIdentifiers(inSection: newIndexPath.section)
+                        if let lastItem = identifiers.last {
+                            snapshot.moveItem(newIndexPath, afterItem: lastItem)
+                        }
+                    }
+                case .update:
+                    if let indexPath = $0.indexPath {
+                        snapshot.reloadItems([indexPath])
+                    }
+                @unknown default:
+                    break
+                }
+            }
+        }
+        updates()
+        
+        // Getting lots of crashes for section animation errors, so let's disable those
+        guard rowChanges.count < 20, sectionChanges.count == 0 else {
+            dataSource.apply(snapshot, animatingDifferences: false)
+            return
+        }
+        dataSource.apply(snapshot, animatingDifferences: true)
+    }
+}
+
+private final class DiffableDataSource: UITableViewDiffableDataSource<Int, IndexPath> {
+    /// header sections
+    var showSectionHeaderTitles = false
+    /// on the right side quick scroll bar
+    var showSectionIndexTitles = true
+    /// For converting sectionIndexTitles
+    public var groupTransformer: (String) -> String = { $0 }
+    let viewHandler: YapViewHandler
+    
+    init(
+        viewHandler: YapViewHandler,
+        tableView: UITableView,
+        cellProvider: @escaping CellProvider
+    ) {
+        self.viewHandler = viewHandler
+        super.init(tableView: tableView, cellProvider: cellProvider)
+    }
+
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
         guard showSectionHeaderTitles else { return nil }
         return groupTransformer(viewHandler.allGroups[section])
     }
     
-    public func sectionIndexTitles(for tableView: UITableView) -> [String]? {
+    override func sectionIndexTitles(for tableView: UITableView) -> [String]? {
         guard showSectionIndexTitles else {
             return nil
         }
@@ -86,41 +244,4 @@ extension YapTableViewAdapter: UITableViewDataSource {
 //        groups.insert(UITableViewIndexSearch, at: 0)
         return groups
     }
-    
-//    public func tableView(_ tableView: UITableView, sectionForSectionIndexTitle title: String, at index: Int) -> Int {
-//        if index > 0 {
-//            return viewHandler.sectionForGroup(title)
-//        } else {
-//            tableView.scrollRectToVisible(CGRect(x: 0, y: 0, width: 1, height: 1), animated: false)
-//            return NSNotFound
-//        }
-//    }
-    
-    public func numberOfSections(in tableView: UITableView) -> Int {
-        return viewHandler.numberOfSections
-    }
-    
-    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return viewHandler.numberOfItemsInSection(section)
-    }
-    
-    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let object = viewHandler.dataObjectAtIndexPath(indexPath) else {
-            return UITableViewCell()
-        }
-        let cell = BRCDataObjectTableViewCell.cell(at: indexPath, tableView: tableView, dataObject: object, writeConnection: writeConnection)
-        return cell
-    }
 }
-
-extension YapTableViewAdapter: YapViewHandlerDelegate {
-    public func didSetupMappings(_ handler: YapViewHandler) {
-        tableView.reloadData()
-    }
-    
-    public func didReceiveChanges(_ handler: YapViewHandler, sectionChanges: [YapDatabaseViewSectionChange], rowChanges: [YapDatabaseViewRowChange]) {
-        tableView.handleYapViewChanges(sectionChanges: sectionChanges, rowChanges: rowChanges)
-    }
-}
-
-
